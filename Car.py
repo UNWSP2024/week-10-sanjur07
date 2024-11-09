4abd87e23b6e50c879430fe62650b80ddfacc2ea
Sanju <sanjanarnath07@gmail.com>
2024-11-08 22:17:57 -0600

    Sanjana Raveendra Nath - 11/8/2024

--- Car.py
+++ Car.py
@@ -1,14 +1,23 @@
-# Program # 2: Car Class
-# Write a class named Car that has the following data attributes:
+class Car:
+    def __int__(self, year_model, make):
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = 0
 
-# __year_model (for the car's year model)
-# __make (for the make of the car)
-# __speed (for the car's current speed)
-# The Car class should have an __init__ method that accepts the car's year model and make as arguments.  These values should be assigned to the object's __year_model and __make data attributes.  It should also assign 0 to the __speed data attribute.
+        def accelerate(self):
+            self.__speed += 5
 
-# The class should also have the following methods:
+        def brake(self):
+            self.__speed -= 5
+        
+        def get_speed(self):
+            return self.__speed
+my_car = Car(2023, "Tesla")
+for i in range(5):
+    my_car.accelerate()
+    print(f"Current speed after accelerating: {my_car.get_speed()} km/h")
+for _ in range(5):
+    my_car.brake()
+    print(f"Current speed after braking: {my_car.get_speed()} km/h")
 
-# The accelerate method should add 5 to the speed data attribute each time it it called.
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# The get_speed method should return the current speed.
-# Next, design a program that creates a Car object then calls the accelerate method five times.  After each call to the accelerate method, get the current speed of the car and display it.  The call the brake method.  After each call to the brake method, get the current speed of the car and display it.
+
--- Employee.py
+++ Employee.py
@@ -1,10 +1,40 @@
-# Program #4 Employee Class:
-# Write a class Employee that holds the following data about an employee in attributes: name, ID number, department, and job title.
+class Employee:
+    def __init__(self, name, id_number, department, job_title):
+        self.name = name
+        self.id_number = id_number
+        self.department = department
+        self.job_title = job_title
 
-# Once you have written the class, write a program that creates three Employee objects to hold the following data.
+    def display(self):
+        print(f"Name: {self.name}")
+        print(f"ID Number: {self.id_number}")
+        print(f"Department: {self.department}")
+        print(f"Job Title: {self.job_title}")
+        print()
 
-# Name	ID Number	Department	Job Title
-# Susan Meyers	47899 	Accounting	Vice President
-# Mark Jones	39119	IT	Programmer
-# Joy Rogers	81774	Manufacturing	Engineer
-# The program should store the data in the three objects, then display the data for each employee on the screen.
+
+employee1 = Employee("Susan Meyers", 65383, "Accounting", "Vice President")
+employee2 = Employee("Mark Jones", 76546, "IT", "Programmer")
+employee3 = Employee("Joy Rogers", 29472, "Manufacturing", "Engineer")
+
+
+employee1.display()
+employee2.display()
+employee3.display()
+'''Start
+               |
+    Create Employee Class
+               |
+        Define __init__ and
+        display methods
+               |
+        Main Program:
+               |
+    Prompt for Employee Details
+               |
+    Create Employee Objects
+               |
+     Display Employee Data
+               |
+              End
+'''
